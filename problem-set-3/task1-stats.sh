dfd62671f6a10644352aef0dc6ad0e2a3bf9fa67
Piotr Szyma <thompson2908@gmail.com>
2018-11-26 22:57:39 +0100

    task 1 works for svn

--- problem-set-3/task1-stats.sh
+++ problem-set-3/task1-stats.sh
@@ -1,34 +1,11 @@
 REPO=$1
-USERNAME=$2
 
-# For listing number of commits
-SVN_LOG=`svn log -v --xml $1`
-NUM_OF_COMMITS=`echo $SVN_LOG | grep '<author>'$2'</author>' | wc -l`
+USERS=`svn log --quiet $REPO | grep "^r" | awk '{print $3}' | sort | uniq`
 
-
-# For getting number of lines added / removed
-
-SUM_ADDED=0
-SUM_DELETED=0
-
-for i in `echo $SVN_LOG | grep -B 1 '<author>'$2'</author>' | awk 'NR % 3 == 1' | grep -Po '\d+'`; do
-
-  if [ "$i" -eq 0 ]
-  then
-    continue
-  fi
-
-  DIFFSTAT=`svn diff https://156.17.7.16/p_25/ -r $PREREV:$REV | diffstat -t`
-  REV=$i
-  PREREV=`expr $REV - 1`
-
-  for ADDED_PER_COMMIT in `echo $DIFFSTAT | awk -F "," '{print $1}' | awk 'NR>1'`; do
-    SUM_ADDED=`expr $SUM_ADDED + $ADDED_PER_COMMIT`
-  done
-  
-  for DELETED_PER_COMMIT in `echo $DIFFSTAT | awk -F "," '{print $2}' | awk 'NR>1'`; do
-    SUM_DELETED=`expr $SUM_DELETED + $DELETED_PER_COMMIT`
-  done
+for USER in $USERS; do
+  echo 'Checking for '$USER
+  bash task1.sh $REPO $USER;
 done
 
-echo 'User '$USERNAME' has made '$NUM_OF_COMMITS' commits; he added '$SUM_ADDED' lines and deleted '$SUM_DELETED' lines.'
+
+
