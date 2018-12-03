103e119ceedadeea3a0d1a9bea32a9e8a34c5920
Piotr Szyma <thompson2908@gmail.com>
2018-12-03 20:53:27 +0100

    Changed

--- problem-set-3/task1.sh
+++ problem-set-3/task1.sh
@@ -14,13 +14,16 @@ NUM_OF_COMMITS=`svn log -v --xml $REPO | grep '<author>'$USERNAME'</author>' | w
 SUM_ADDED=0
 SUM_DELETED=0
 
+# Loop through author's revisions.
 for i in `svn log -v --xml $REPO | grep -B 1 '<author>'$USERNAME'</author>' | awk 'NR % 3 == 1' | grep -Po '\d+'`; do
   REV=$i
 
+  # Get lines added in revision using diffstat.
   for ADDED_PER_COMMIT in `svn diff $REPO -c $REV | diffstat -t | awk -F "," '{print $1}' | awk 'NR>1' | awk '{s+=$1} END {print s}'`; do
     SUM_ADDED=`expr $SUM_ADDED + $ADDED_PER_COMMIT`
   done
-  
+
+  # Get lines deleted in revision using diffstat.  
   for DELETED_PER_COMMIT in `svn diff $REPO -c $REV | diffstat -t | awk -F "," '{print $2}' | awk 'NR>1' | awk '{s+=$1} END {print s}'`; do
     SUM_DELETED=`expr $SUM_DELETED + $DELETED_PER_COMMIT`
   done
