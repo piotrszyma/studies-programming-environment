eae85f7a84f2ec642e451246d17e3061f2ec8bc0
Piotr Szyma <thompson2908@gmail.com>
2018-12-03 20:41:12 +0100

    fixed lines changed count

--- problem-set-3/task1.sh
+++ problem-set-3/task1.sh
@@ -8,20 +8,20 @@ REPO=$1
 USERNAME=$2
 
 # For listing number of commits.
-NUM_OF_COMMITS=`svn log -v --xml $1 | grep '<author>'$2'</author>' | wc -l`
+NUM_OF_COMMITS=`svn log -v --xml $REPO | grep '<author>'$USERNAME'</author>' | wc -l`
 
 # For getting number of lines added / removed.
 SUM_ADDED=0
 SUM_DELETED=0
 
-for i in `svn log -v --xml $1 | grep -B 1 '<author>'$2'</author>' | awk 'NR % 3 == 1' | grep -Po '\d+'`; do
+for i in `svn log -v --xml $REPO | grep -B 1 '<author>'$USERNAME'</author>' | awk 'NR % 3 == 1' | grep -Po '\d+'`; do
   REV=$i
 
-  for ADDED_PER_COMMIT in `svn diff https://156.17.7.16/p_25/  -c $REV | diffstat -t | awk -F "," '{print $1}' | awk 'NR>1'`; do
+  for ADDED_PER_COMMIT in `svn diff $REPO -c $REV | diffstat -t | awk -F "," '{print $1}' | awk 'NR>1' | awk '{s+=$1} END {print s}'`; do
     SUM_ADDED=`expr $SUM_ADDED + $ADDED_PER_COMMIT`
   done
   
-  for DELETED_PER_COMMIT in `svn diff https://156.17.7.16/p_25/  -c $REV | diffstat -t | awk -F "," '{print $2}' | awk 'NR>1'`; do
+  for DELETED_PER_COMMIT in `svn diff $REPO -c $REV | diffstat -t | awk -F "," '{print $2}' | awk 'NR>1' | awk '{s+=$1} END {print s}'`; do
     SUM_DELETED=`expr $SUM_DELETED + $DELETED_PER_COMMIT`
   done
 done
