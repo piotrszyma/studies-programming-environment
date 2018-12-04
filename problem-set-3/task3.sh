2925b6fd26fda7e92aee469b52e66e6363313763
Piotr Szyma <thompson2908@gmail.com>
2018-12-04 20:19:55 +0100

    task3 fixed

--- problem-set-3/task3.sh
+++ problem-set-3/task3.sh
@@ -1,5 +1,5 @@
 # Cache current branch.
-$CURRENT_BRANCH=`git branch | grep \* | cut -d ' ' -f2`
+CURRENT_BRANCH=`git branch | grep \* | cut -d ' ' -f2`
 
 # Get unreachable commit's objects' hashes and create branch for each.
 for HASH in `git fsck --full --no-reflogs | grep commit | awk '{ print $3 }'`; do
